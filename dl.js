211a527a37de45d5faece716272ba18e98102566
kentaro19781123 <kens19781123@gmail.com>
2021-08-28 18:34:15 +0900

    dl

--- dl.js
+++ dl.js
@@ -1,15 +1,16 @@
 const fs = require('fs');
 const request = require('request');
 
+const imageDir = 'image'
 const download = function(uri, filename, callback) {
-  if(!fs.existsSync(filename)) {
+  if (!fs.existsSync(filename)) {
     // ファイルがなかったら
     console.log("ない");
-
     request.head(uri, function(err, res, body) {
       console.log('statusCode:', res && res.statusCode);
       if(res.statusCode === 200) {
         console.log(res.statusCode);
+        if (!fs.existsSync(imageDir)) fs.mkdirSync(imageDir);
         request
         .get(uri)
         .pipe(fs.createWriteStream(filename))
@@ -23,6 +24,4 @@ const download = function(uri, filename, callback) {
   }
 };
 
-download('https://www.google.com/images/srpr/logo3w.png', './images/google.png', function() {
-  console.log('download end');
-});
+download('https://www.google.com/images/srpr/logo3w.png', `${imageDir}/google.png`, () => console.log('download end'));
